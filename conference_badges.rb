3712f6465125fc3e1b0c8ee10f4a22dca281d113
Evan Jeffries <ejeffries14@gmail.com>
2019-06-19 17:33:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(array)
   new_array = []
-  new_array.unshift ("Hello, my name is #{attendees}.")
+  new_array.unshift ("Hello, my name is #{array}.")
   return new_array
 end
 
