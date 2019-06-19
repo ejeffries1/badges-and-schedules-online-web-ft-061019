5394b45980ab502c2d60be93e1ecfe4b6ebf3e6d
Evan Jeffries <ejeffries14@gmail.com>
2019-06-19 17:29:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  new_array.unshift ("Hello, my name is #{attendees}.")
+  return new_array
+end
+
+def assign_rooms(attendees)
+array = []
+attendees.each do |name|
+  puts "Hello, #{attendees}! You'll be assigned to room #{attendees.index}!"
+  return array.push(name)
+end
